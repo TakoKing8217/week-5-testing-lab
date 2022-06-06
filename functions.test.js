9c0fd0d2ec55240424403c4d6484715f73c257ee
Nathan Wood <nathan.ronald.wood@gmail.com>
2022-06-06 15:13:01 -0500

    testing-lab completed

--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,11 @@
-const { returnTwo, greeting, add } = require("./functions");
+const {
+  returnTwo,
+  greeting,
+  add,
+  subtract,
+  multiply,
+  divide,
+} = require("./functions");
 
 test("returnTwo should return 2", () => {
   expect(returnTwo()).toBe(2);
@@ -9,7 +16,21 @@ test("greeting should expect a nice greeting for the name", () => {
   expect(greeting("James")).toBe("Hello, James.");
 });
 
-test("the function add should add the numbers together", () => {
-  expect(add(1, 2)).toBe(3);
-  expect(add(5, 9)).toBe(14);
+describe("do the math functions work", () => {
+  test("the add function works", () => {
+    expect(add(1, 2)).toBe(3);
+    expect(add(5, 9)).toBe(14);
+  });
+  test("the subtract function works", () => {
+    expect(subtract(19, 9)).toBe(10);
+    expect(subtract(2, 1)).toBe(1);
+  });
+  test("the multiply function works", () => {
+    expect(multiply(5, 9)).toBe(45);
+    expect(multiply(4, 2)).toBe(8);
+  });
+  test("the divide function works", () => {
+    expect(divide(100, 5)).toBe(20);
+    expect(divide(5, 5)).toBe(1);
+  });
 });
